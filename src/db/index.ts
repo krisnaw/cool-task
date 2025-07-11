67fc33f8cd8daa0ea3c81b986d924726996385c5
krisna <krisna.w2010@gmail.com>
2025-07-11 12:13:26 +0800

    wip

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,10 +1,13 @@
 import { createClient } from "@libsql/client";
-import { drizzle } from "drizzle-orm/singlestore";
+import { drizzle } from "drizzle-orm/libsql";
 
+import * as schema from "@/db/schema";
 import env from "@/env";
 
 const client = createClient({ url: env.DATABASE_URL });
 
-const db = drizzle(client);
+const db = drizzle(client, {
+  schema,
+});
 
 export default db;
